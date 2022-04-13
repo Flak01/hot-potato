e824eed60a2fd860b8ee645ab8fd47c0c860cdac
Flak01 <102991631+Flak01@users.noreply.github.com>
2022-04-13 13:12:20 -0600

    im done

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-let marcus = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showString("3")
-    basic.showString("2")
-    basic.showString("1")
+    strip.clear()
+    var3.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(500)
+    strip.clear()
+    var2.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(500)
+    strip.clear()
+    var1.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(500)
     marcus = randint(5, 15)
     while (marcus > 0) {
         basic.clearScreen()
@@ -125,6 +130,16 @@ input.onButtonPressed(Button.A, function () {
         basic.showIcon(IconNames.Silly)
     }
 })
+let marcus = 0
+let var1: neopixel.Strip = null
+let var2: neopixel.Strip = null
+let var3: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
+var3 = strip.range(11, 15)
+var2 = strip.range(6, 10)
+var1 = strip.range(0, 5)
+strip.showColor(neopixel.rgb(randint(0, 64), randint(0, 64), randint(0, 64)))
 basic.forever(function () {
 	
 })
